7b310ebd09a02fec1543996f680752f7bd91e302
hainguyen2k <ngxuanhai1000@gmail.com>
2021-12-12 21:18:17 +0700

    Update retry, cache spring boot

--- src/main/java/com/example/subproject_kttkpm_14btt_haibaotunghiahuy_student/SubprojectKttkpm14BttHaiBaoTuNghiaHuyStudentApplication.java
+++ src/main/java/com/example/subproject_kttkpm_14btt_haibaotunghiahuy_student/SubprojectKttkpm14BttHaiBaoTuNghiaHuyStudentApplication.java
@@ -2,8 +2,12 @@ package com.example.subproject_kttkpm_14btt_haibaotunghiahuy_student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableCaching
 public class SubprojectKttkpm14BttHaiBaoTuNghiaHuyStudentApplication {
 
     public static void main(String[] args) {
